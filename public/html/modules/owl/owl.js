ef4de6e0df822dc3dc48f40b303d1fda733d0845
pengcl <3559996@qq.com>
2019-10-05 15:24:35 +0800

    update

--- public/html/modules/owl/owl.js
+++ public/html/modules/owl/owl.js
@@ -1,18 +1,20 @@
 'use strict';
 
 app.directive('ngOwl', ['$location', 'appSvc', function ($location, appSvc) {
-  return {
-    restrict: 'C',
-    scope: {},
-    templateUrl: 'modules/owl/owl.html',
-    link: function (scope, element, attrs) {
-      $(".owl-carousel").owlCarousel({
-        navigation: true, // Show next and prev buttons
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true,
-        autoPlay: 3000
-      });
-    }
-  };
+    return {
+        restrict: 'C',
+        scope: {},
+        templateUrl: 'modules/owl/owl.html',
+        link: function (scope, element, attrs) {
+            $(".owl-carousel").owlCarousel({
+                navigation: true, // Show next and prev buttons
+                slideSpeed: 300,
+                loop: true,
+                paginationSpeed: 400,
+                singleItem: true,
+                autoPlay: 3000,
+                items: 1
+            });
+        }
+    };
 }]);
--- public/html/modules/top/top.js
+++ public/html/modules/top/top.js
@@ -1,14 +1,23 @@
 'use strict';
 
 app.directive('ngTop', ['$location', 'appSvc', function ($location, appSvc) {
-  return {
-    restrict: 'C',
-    scope: {},
-    templateUrl: 'modules/top/top.html',
-    link: function (scope, element, attrs) {
-      scope.onClick = function () {
-        toTop();
-      };
-    }
-  };
+    return {
+        restrict: 'C',
+        scope: {},
+        templateUrl: 'modules/top/top.html',
+        link: function (scope, element, attrs) {
+            scope.show = false;
+            $(window).on('scroll', function (e) {
+                if (e.currentTarget.scrollY > e.currentTarget.innerHeight) {
+                    scope.show = true;
+                } else {
+                    scope.show = false;
+                }
+              scope.$apply();
+            });
+            scope.onClick = function () {
+                toTop();
+            };
+        }
+    };
 }]);
