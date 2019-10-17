d35f89c6e4cfd5be503507e759a53ca09dd7b4c2
purrck <493715706@qq.com>
2019-10-17 11:30:09 +0800

    1111

--- public/html/modules/footer/footer.js
+++ public/html/modules/footer/footer.js
@@ -1,11 +1,13 @@
 'use strict';
 
 app.directive('ngFooter', ['$location', function ($location) {
-  return {
-    restrict: 'C',
-    scope: {},
-    templateUrl: 'modules/footer/footer.html',
-    link: function (scope, element, attrs) {
-    }
-  };
+    return {
+        restrict: 'C',
+        scope: {
+            contactShow: true
+        },
+        templateUrl: 'modules/footer/footer.html',
+        link: function (scope, element, attrs) {
+        }
+    };
 }]);
