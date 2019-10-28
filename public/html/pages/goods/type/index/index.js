4a36019ab48a7795483577de955a379c96893ac4
pengcl <3559996@qq.com>
2019-10-28 14:58:42 +0800

    update

---
+++ public/html/pages/goods/type/index/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    // 设定路由
+    $routeProvider
+        .when('/goods/type/index', { //app首页
+            templateUrl: 'pages/goods/type/index/index.html',
+            controller: "goodsTypeIndexController"
+        });
+}]).controller('goodsTypeIndexController', ['$scope', '$routeParams', '$location', 'productSvc', function ($scope, $routeParams, $location, productSvc) {
+
+}]);
